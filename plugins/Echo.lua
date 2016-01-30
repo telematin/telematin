bce9356e06545beb9e2e2cc986999a2f4cfab7f4
telematin <webamoz1380@gmail.com>
2016-01-30 16:42:25 +0330

    Update Echo.lua

--- plugins/Echo.lua
+++ plugins/Echo.lua
@@ -1,21 +1,18 @@
 
 local function run(msg, matches)
-  local text = matches[1]
-  local b = 1
-
-  while b ~= 0 do
-    text = text:trim()
-    text,b = text:gsub('^!+','')
-  end
-  return text
+local text = matches[1]
+local b = 1
+while b ~= 0 do
+text = text:trim()
+text,b = text:gsub('^!+','')
 end
-
+return text
+end
 return {
-  description = "Reply Your Sent Message",
-  usage = "/echo (message) : reply message",
-  patterns = {
-    "^[!/]echo +(.+)$"
-  }, 
-	run = run,
-	moderated = true
+description = "Simplest plugin ever!",
+usage = "!echo [whatever]: echoes the msg",
+patterns = {
+"^!echo +(.+)$"
+},
+run = run
 }
